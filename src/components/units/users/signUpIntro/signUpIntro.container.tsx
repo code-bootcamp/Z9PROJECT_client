48ebda4c037e3d18d669408cf3d0da0ce93bc1ba
idealstring <idealstring@gmail.com>
2022-11-01 22:59:24 +0900

    FEATURE: 회원가입 인트로 페이지 퍼블리싱#8
    회원가입 인트로 페이지 퍼블리싱
    크리에이터 회원가입 페이지 퍼블리싱
    
    closed #8

---
+++ src/components/units/users/signUpIntro/signUpIntro.container.tsx
@@ -0,0 +1,75 @@
+import styled from "@emotion/styled";
+import Link from "next/link";
+
+const Container = styled.section`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const LinkTag = styled.a`
+  width: 100%;
+  height: 100%;
+`;
+
+const CreatorBtn = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  font-size: 2rem;
+  background: rgba(43, 45, 66, 1);
+  color: white;
+  span {
+    font-size: 0.9rem;
+  }
+`;
+
+const UserBtn = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  color: black;
+
+  font-size: 2rem;
+  span {
+    font-size: 0.9rem;
+  }
+`;
+
+export default function SignUpIntroContainer() {
+  return (
+    <Container>
+      <Link href={"/users/signupintro/creator"}>
+        <LinkTag>
+          <CreatorBtn>
+            <img src="/users/signupintro/img_bulb.png" />
+            크리에이터
+            <span>회원가입</span>
+          </CreatorBtn>
+        </LinkTag>
+      </Link>
+      <Link href={"/users/signupintro/user"}>
+        <LinkTag>
+          <UserBtn>
+            <img src="/users/signupintro/img_register.png" />
+            일반회원
+            <span>회원가입</span>
+          </UserBtn>
+        </LinkTag>
+      </Link>
+    </Container>
+  );
+}
