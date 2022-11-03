860aff25eec0ce17b94ab7dd294b66de627c1dd0
idealstring <idealstring@gmail.com>
2022-11-03 14:09:58 +0900

    FEATURE : 크리에이터 회원가입 페이지 디자인 수정 #22
    input border 값 수정
    yup 오류 메세지 적용
    오류 메세지 애니메이션 적용
    크리에이터 정보 단락 설정
    
    closed #22

--- src/components/commons/input/input01/input01.tsx
+++ src/components/commons/input/input01/input01.tsx
@@ -1,25 +1,74 @@
 import styled from "@emotion/styled";
 import {
-  FieldValues,
-  UseFormRegister,
+  FieldError,
+  FieldErrorsImpl,
+  Merge,
   UseFormRegisterReturn,
 } from "react-hook-form";
+import { styleSet } from "../../../../commons/styles/styleSet";
+
+const Wrapper = styled.div`
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+`;
 
 const Input = styled.input`
-  padding: 12px 12px 12px 40px;
+  padding: 20px 20px 20px 55px;
   width: 100%;
-  border: none;
-  border-bottom: 1px solid #000000;
+  border-radius: 10px;
+  border: 1px solid ${styleSet.colors.gray};
+  outline-color: ${styleSet.colors.primary};
+
+  border-color: ${(P: IInputProps) =>
+    P.error ? `${styleSet.colors.primary}` : `${styleSet.colors.gray}`};
+`;
+
+const Error = styled.div`
+  position: absolute;
+  top: 50%;
+  right: 1rem;
+  color: ${styleSet.colors.primary};
+  transition: all 0.35s;
+
+  transform: ${(P: IInputProps) =>
+    P.error ? `translate(0, -50%)` : `translate(5rem, -50%)`};
+  opacity: ${(P: IInputProps) => (P.error ? `1` : `0`)};
 `;
 
 type IInput01Props = {
   type: string;
   placeholder: string;
   register: UseFormRegisterReturn;
+  error?:
+    | string
+    | FieldError
+    | Merge<FieldError, FieldErrorsImpl<any>>
+    | undefined;
+};
+
+type IInputProps = {
+  error?:
+    | string
+    | FieldError
+    | Merge<FieldError, FieldErrorsImpl<any>>
+    | undefined;
 };
 
 export default function Input01(P: IInput01Props) {
-  const { type, placeholder, register } = P;
+  const { type, placeholder, register, error } = P;
 
-  return <Input type={type} placeholder={placeholder} {...register} />;
+  return (
+    <Wrapper>
+      <Input
+        type={type}
+        placeholder={placeholder}
+        {...register}
+        error={error}
+      />
+      <Error className="error" error={error}>
+        {error}
+      </Error>
+    </Wrapper>
+  );
 }
