7de9ca2dc3349e02c9ddefc72115d206e50b2a9e
Brian Hong <ushiii111798@gmail.com>
2022-11-10 21:33:19 +0900

    HOTFIX: type declare for setSwiperRef

--- src/components/units/main/main.types.ts
+++ src/components/units/main/main.types.ts
@@ -3,5 +3,6 @@ import { Dispatch, SetStateAction } from "react";
 export type IMainPresenterProps = {
   handleLeftClick: () => void;
   handleRightClick: () => void;
-  setSwiperRef: Dispatch<SetStateAction<undefined>>;
+  // setSwiperRef: Dispatch<SetStateAction<undefined>>;
+  setSwiperRef: any;
 };
