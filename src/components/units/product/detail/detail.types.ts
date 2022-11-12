e0d90d3f6418f61012c92924b90f3d73a04ae05b
idealstring <idealstring@gmail.com>
2022-11-12 17:41:31 +0900

    HOTFIX: update dedailPage type

--- src/components/units/product/detail/detail.types.ts
+++ src/components/units/product/detail/detail.types.ts
@@ -1,16 +1,16 @@
 import { MouseEvent } from "react";
 
 export type IDetailPresenterProps = {
-  handleChange?: (value: unknown) => void;
+  handleChange?: ((value: string) => void) | undefined;
   onClickCount?: (e: MouseEvent<HTMLButtonElement>) => void;
   count: number;
   onClickCart?: () => void;
   cart: boolean;
   data?: any;
   discount?: number;
-  onClickLike: (productId: any) => Promise<void>;
-  thumbnail: any;
-  onClickImages: any;
-  onClickAnswer: () => void;
-  isTrue: boolean;
+  onClickLike?: (productId: any) => Promise<void>;
+  thumbnail?: any;
+  onClickImages?: any;
+  onClickAnswer?: () => void;
+  isTrue?: boolean;
 };
