84c03a005ef2d832ca0634845feb7c69633f2010
smyn95 <tlsalduszz@gmail.com>
2022-11-14 01:07:38 +0900

    FEATURE : 답변 API 적용
    답변 등록,수정 완료
    
    closed #172

--- src/components/units/answer/list/answerList.tsx
+++ src/components/units/answer/list/answerList.tsx
@@ -1,28 +1,49 @@
 import { IQnaProps } from "../../question/question.types";
 import * as S from "../answer.styles";
+import AnswerWriter from "../write/answerWriter";
 
 export default function AnswerList(P: IQnaProps) {
-  const { el } = P;
+  const { el, dom, isAnswer, setIsAnswer, setAnswerModal, isTrue } = P;
+
+  const onClickEditAnswer = () => {
+    setIsAnswer(true);
+  };
   return (
     <>
       <S.User>
-        <S.AnswerBox>
-          <S.Div>
-            <S.Contents>{el.question}</S.Contents>
-            <S.Text>
-              <S.AnswerContents>
-                <span>답변</span>
-                어쩌라구여어쩌라구여어쩌라구여어쩌라구여어쩌라구여어쩌라구여어쩌라구여어쩌라구여어쩌라구여어쩌라구여어쩌라구여어쩌라구여어쩌라구여어쩌라구여어쩌라구여어쩌라구여어쩌라구여어쩌라구여
-              </S.AnswerContents>
-            </S.Text>
-            <S.Answer>
-              <S.UserInfo>Leo J</S.UserInfo>
-              <S.UserInfo>2022.11.03</S.UserInfo>
-              <button>수정</button>
-              <button>삭제</button>
-            </S.Answer>
-          </S.Div>
-        </S.AnswerBox>
+        {isTrue && (
+          <S.AnswerBox>
+            <S.Div>
+              <S.Contents>{el.question}</S.Contents>
+              <S.Text>
+                <S.AnswerContents>
+                  <span>답변</span>
+                  {dom.answer}
+                </S.AnswerContents>
+              </S.Text>
+              <S.Answer>
+                <S.UserInfo>Leo J </S.UserInfo>
+                <S.UserInfo>
+                  {dom?.createdAt
+                    ?.slice(0, 10)
+                    .replace("-", "년")
+                    .replace("-", "월")}
+                  일
+                </S.UserInfo>
+                <button onClick={onClickEditAnswer}>수정</button>
+              </S.Answer>
+            </S.Div>
+          </S.AnswerBox>
+        )}
+        {isAnswer && (
+          <AnswerWriter
+            setAnswerModal={setAnswerModal}
+            questionEl={el}
+            isAnswer={true}
+            dom={dom}
+            setIsAnswer={setIsAnswer}
+          />
+        )}
       </S.User>
     </>
   );
