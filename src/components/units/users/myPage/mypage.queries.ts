c07d5c9a90861035752d817a0a0c4d5bd6a58ee6
smyn95 <72180863+smyn95@users.noreply.github.com>
2022-11-14 23:00:24 +0900

    Feature mypage (#188)
    * INIT: responsive setting
    
    반응형 퍼블리싱 세팅
    
    * 리스트페이지 반응형 수정
    
    * Merge branch 'develop' of https://github.com/code-bootcamp/offline_9t… (#1)
    
    * BUILD: CI: Update to SSR Depoly
    
    * HOTFIX: type not include fix
    
    * HOTFIX: Add IMP type
    
    * HOTFIX: add temporary type for swiperRef
    
    * FEATURE : 마이페이지 프로필 수정 기능 #145
    
    비밀번호 변경페이지 추가(+기능))
    사이드바 비밀번호 변경 버튼 추가
    프로필 수정 기능
    
    closed #145
    
    * 상세페이지 2개의 테마 반응형 퍼블리싱 (#150)
    
    모바일 햄버거메뉴 수정
    
    closed #97
    
    * Feature prod detail first (#152)
    
    * 상세페이지 2개의 테마 반응형 퍼블리싱
    모바일 햄버거메뉴 수정
    
    closed #97
    
    * Update useMoveToPage.tsx
    
    * Feature prod detail first (#154)
    
    * 상세페이지 2개의 테마 반응형 퍼블리싱
    모바일 햄버거메뉴 수정
    
    closed #97
    
    * Update useMoveToPage.tsx
    
    * Feature prod detail first (#155)
    
    * 상세페이지 2개의 테마 반응형 퍼블리싱
    모바일 햄버거메뉴 수정
    
    closed #97
    
    * FEATURE: 상세페이지 FETCH(#146)
    
    상세페이지 FETCH
    
    * Update useMoveToPage.tsx
    
    * FEATURE : 마이페이지 찜목록 #151
    
    찜목록 fetch
    더보기 버튼 기능
    헤더 모바일버전 메뉴 포인트 추가
    
    closed #151
    
    Co-authored-by: Brian Hong <ushiii111798@gmail.com>
    Co-authored-by: idealstring <idealstring@gmail.com>

--- src/components/units/users/myPage/mypage.queries.ts
+++ src/components/units/users/myPage/mypage.queries.ts
@@ -4,9 +4,47 @@ export const FETCH_USER = gql`
   query fetchUser {
     fetchUser {
       id
+      email
+      phoneNumber
+      address
+      zipcode
       userType
       nickname
       point
+    }
+  }
+`;
+
+export const CREATE_PAYMENT = gql`
+  mutation createPayment($impUid: String!, $amount: Float!) {
+    createPayment(impUid: $impUid, amount: $amount) {
+      id
+      point {
+        id
+        point
+        status
+      }
+    }
+  }
+`;
+
+export const POINT_HISTORY_COUNT = gql`
+  query fetchCountOfPointHistory($startDate: DateTime, $endDate: DateTime) {
+    fetchCountOfPointHistory(startDate: $startDate, endDate: $endDate)
+  }
+`;
+
+export const POINT_HISTORY = gql`
+  query fetchPointHistory(
+    $startDate: DateTime
+    $endDate: DateTime
+    $page: Float!
+  ) {
+    fetchPointHistory(startDate: $startDate, endDate: $endDate, page: $page) {
+      id
+      point
+      status
+      createdAt
     }
   }
 `;
