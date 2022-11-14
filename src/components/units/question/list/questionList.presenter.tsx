353f113613f97eb87a929a550324200b8054d174
smyn95 <72180863+smyn95@users.noreply.github.com>
2022-11-14 20:28:15 +0900

    Feature qna (#183)
    * FEATRUE: 질문 API 적용
    
    * FEATURE : 답변 API 적용
    
    답변 등록,수정 완료
    
    closed #172
    
    * FIX: qna 컴포넌트 분리(#176)
    
    질문답변 쿼리 수정 이후 정리
    
    closed #176

--- src/components/units/question/list/questionList.presenter.tsx
+++ src/components/units/question/list/questionList.presenter.tsx
@@ -1,10 +1,15 @@
 import { useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
 import { FETCH_QUESTIONS } from "../question.queries";
 import QuestionList from "./questionList";
 
 export default function QuestionPresenter(P: any) {
+  const router = useRouter();
   const { onClickAnswer, isTrue } = P;
-  const { data } = useQuery(FETCH_QUESTIONS);
+  const { data } = useQuery(FETCH_QUESTIONS, {
+    fetchPolicy: "network-only",
+    variables: { productId: String(router.query.useditemId) },
+  });
 
   return (
     <>
