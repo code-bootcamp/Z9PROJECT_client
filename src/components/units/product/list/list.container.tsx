178aef16ba073c2574614b355d950ab254a3ab49
smyn95 <72180863+smyn95@users.noreply.github.com>
2022-11-15 14:43:42 +0900

    Feature prod list (#194)
    * FEATURE: 리스트 페이지 퍼블리싱(#17)
    
    리스트페이지 퍼블리싱
    레이아웃 (헤더,푸터) 퍼블리싱 헤더는 fixed로 인해 모든 페이지에서 padding-top을 주어야 함
    메인 css 에서 padding-top 추가
    
    closed #17
    
    * Issue 76 responsive list (#84)

--- src/components/units/product/list/list.container.tsx
+++ src/components/units/product/list/list.container.tsx
@@ -1,9 +1,46 @@
-import ProductListPresenter from "./list.presenter";
+import { useQuery } from "@apollo/client";
+import { useState } from "react";
+import ProductListMap from "./list.map";
+import { FETCH_PRODUCTS_BY_PAGES } from "./list.queries";
 
 export default function ProductListContainer() {
+  const [tab, setTab] = useState<any>("1");
+
+  const { data, fetchMore } = useQuery(FETCH_PRODUCTS_BY_PAGES, {
+    fetchPolicy: "network-only",
+    variables: { page: 1 },
+  });
+
+  const onClickTab = (event: any) => {
+    setTab(event?.currentTarget.id);
+  };
+
+  const onLoadMore = () => {
+    if (!data) return;
+
+    void fetchMore({
+      variables: { page: Math.ceil(data?.fetchProductsByPages.length / 4) + 1 },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult?.fetchProductsByPages)
+          return { fetchProductsByPages: [...prev.fetchProductsByPages] };
+        return {
+          fetchProductsByPages: [
+            ...prev.fetchProductsByPages,
+            ...fetchMoreResult.fetchProductsByPages,
+          ],
+        };
+      },
+    });
+  };
+
   return (
     <>
-      <ProductListPresenter />
+      <ProductListMap
+        tab={tab}
+        onClickTab={onClickTab}
+        data={data}
+        onLoadMore={onLoadMore}
+      />
     </>
   );
 }
