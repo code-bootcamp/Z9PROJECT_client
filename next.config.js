6bee042dfe5a7b786025c7ab029d3bef584a28ff
idealstring <idealstring@gmail.com>
2022-11-18 21:29:38 +0900

    FEATURE : PWA 추가 #262
    PWA 추가
    
    closed #262

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  // disable: process.env.NODE_ENV == "development",
+});
+// import withPlugins from "next-compose-plugins";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  trailingSlash: true,
+};
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+// module.exports = withPlugins(
+//   [
+//     [
+//       withPWA,
+//       {
+//         pwa: {
+//           dest: "public",
+//         },
+//       },
+//     ],
+//     // More Plugin
+//   ],
+//   nextConfig
+// );
+module.exports = withPWA();
