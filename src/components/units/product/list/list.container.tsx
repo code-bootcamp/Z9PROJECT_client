53d84653a94fead691fefc0379107ea578cb22a0
smyn95 <tlsalduszz@gmail.com>
2022-11-18 16:40:11 +0900

    delete infiniteScroll

--- src/components/units/product/list/list.container.tsx
+++ src/components/units/product/list/list.container.tsx
@@ -5,7 +5,6 @@ import { FETCH_PRODUCTS_BY_PAGES } from "./list.queries";
 
 export default function ProductListContainer() {
   const [tab, setTab] = useState<any>("1");
-  const [length, setLength] = useState(0);
 
   const { data, fetchMore } = useQuery(FETCH_PRODUCTS_BY_PAGES, {
     fetchPolicy: "network-only",
@@ -35,13 +34,11 @@ export default function ProductListContainer() {
       },
     });
   };
-  console.log(data);
   return (
     <>
       <ProductListMap
         onClickTab={onClickTab}
         onLoadMore={onLoadMore}
-        length={length}
         tab={tab}
         data={data}
       />
